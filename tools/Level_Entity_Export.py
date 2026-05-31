a52f0c8677102502348e3dd7fba5a9196476de5b
Gabe <gabejstein@gmail.com>
2026-05-30 21:29:03 -0400

    Worked on level export script some more.

--- tools/Level_Entity_Export.py
+++ tools/Level_Entity_Export.py
@@ -12,7 +12,12 @@ import struct
 def WriteVec3(file,v):
     file.write(struct.pack('f',v[0]))
     file.write(struct.pack('f',v[2]))
-    file.write(struct.pack('f',v[1]))
+    file.write(struct.pack('f',v[1]*-1)) #Changing the order for Raylib
+    
+def WriteBoundingBox(file,v):
+    file.write(struct.pack('f',v[0]))
+    file.write(struct.pack('f',v[2]))
+    file.write(struct.pack('f',v[1])) #Changing the order for Raylib
     
 def WriteString(file,s):
     file.write(struct.pack('I',len(s)))
@@ -49,9 +54,12 @@ billboardList = [obj for obj in entityCollection.objects if "billboard" in obj]
 billboardCount = len(billboardList)
 
 #Get meta data
+#levelMetaData = none
 for obj in entityCollection.objects:
     if obj.name=="meta_data":
-        levelName = obj["level_name"]     
+        levelName = obj["level_name"]
+        #levelMetaData = obj
+        break   
         
 fileName = baseDir+"\\"+levelName+fileExt
 
@@ -63,12 +71,14 @@ file.write(struct.pack('I',entityCount))
 file.write(struct.pack('I',portalCount))
 file.write(struct.pack('I',spawnPointCount))
 file.write(struct.pack('I',billboardCount))
+#WriteString(file,levelMetaData["model_path"]) #TODO: make model export path the same
 
 #save portals
 for obj in portalsList:
     WriteString(file,obj["portal"]) #level id
     WriteString(file,obj["spawn_id"])
-    WriteVec3(file,obj.dimensions) #bounding box
+    WriteVec3(file,obj.location)
+    WriteBoundingBox(file,obj.dimensions) #bounding box
     
 for obj in spawnPointList:
     WriteString(file,obj["spawn_point"]) #id string
@@ -84,9 +94,7 @@ for obj in entitiesList:
     WriteString(file,obj["class"]) 
     
     #position
-    file.write(struct.pack('f',obj.location[0]))
-    file.write(struct.pack('f',obj.location[2])) #Changing the order for Raylib
-    file.write(struct.pack('f',obj.location[1]*-1))
+    WriteVec3(file,obj.location)
     
     #rotation
     file.write(struct.pack('f',obj.rotation_euler[2])) #exports in radians
